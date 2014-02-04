149aa7e5de58a9ec128bd091fc285a29f66994d9
Andrew Hollenbach <hollenbach.andrew@rit.edu>
2014-02-04 12:05:24 -0500

    code clean-up
    removed no_commit folder (duh), fixed some links, re-centered logo

--- js/logo.js
+++ js/logo.js
@@ -2,53 +2,56 @@ $(document).ready(function() {
     var canvas = document.getElementById('logo');
     canvas.width = 150;
     canvas.height = 100;
-    var context = canvas.getContext('2d');
+    var ctx = canvas.getContext('2d');
+    var fontColor = "rgb(238,238,238)";
 
-    // begin custom shape
-    context.beginPath();
-    context.moveTo(5, 75);
-    context.lineTo(32,6);
-    context.lineTo(44,6);
-    context.lineTo(75,80);
-    context.lineTo(144,80);
-    context.lineTo(144,90);
-    context.lineTo(65,90);
-    context.lineTo(52,59);
-    context.lineTo(25,59);
-    context.lineTo(19,75);
+    // Begin "A"
+    ctx.beginPath();
+    ctx.moveTo(5, 75);
+    ctx.lineTo(32,6);
+    ctx.lineTo(44,6);
+    ctx.lineTo(75,80);
+    ctx.lineTo(144,80);
+    ctx.lineTo(144,90);
+    ctx.lineTo(65,90);
+    ctx.lineTo(52,59);
+    ctx.lineTo(25,59);
+    ctx.lineTo(19,75);
 
-    // complete custom shape
-    context.closePath();
-    context.fillStyle = "rgb(238,238,238)";
-    context.fill();
+    // Complete "A"
+    ctx.closePath();
+    ctx.fillStyle = fontColor;
+    ctx.fill();
 
-    var oldCompOp = context.globalCompositeOperation;
-    context.globalCompositeOperation = "xor";
+    // Remove center of "A" (XOR it out)
+    var textCompOp = ctx.globalCompositeOperation;
+    ctx.globalCompositeOperation = "xor";
 
-    context.beginPath();
-    context.moveTo(28, 48);
-    context.lineTo(38, 22);
-    context.lineTo(48, 48);
-    context.closePath();
-    context.fillStyle = "rgba(0,0,0,1)";
-    context.fill();
+    ctx.beginPath();
+    ctx.moveTo(28, 48);
+    ctx.lineTo(38, 22);
+    ctx.lineTo(48, 48);
+    ctx.closePath();
+    ctx.fillStyle = "rgba(0,0,0,1)";
+    ctx.fill();
 
-    context.globalCompositeOperation = oldCompOp;
+    // Begin "H"
+    ctx.globalCompositeOperation = textCompOp;
 
-    context.beginPath();
-    context.moveTo(81, 75);
-    context.lineTo(81,6);
-    context.lineTo(95,6);
-    context.lineTo(95,33);
-    context.lineTo(121,33);
-    context.lineTo(121,6);
-    context.lineTo(135,6);
-    context.lineTo(135,75);
-    context.lineTo(121,75);
-    context.lineTo(121,47);
-    context.lineTo(95,47);
-    context.lineTo(95,75);
-    context.closePath();
-    context.fillStyle = "rgb(238,238,238)";
-    context.fill();
+    ctx.beginPath();
+    ctx.moveTo(81, 75);
+    ctx.lineTo(81,6);
+    ctx.lineTo(95,6);
+    ctx.lineTo(95,33);
+    ctx.lineTo(121,33);
+    ctx.lineTo(121,6);
+    ctx.lineTo(135,6);
+    ctx.lineTo(135,75);
+    ctx.lineTo(121,75);
+    ctx.lineTo(121,47);
+    ctx.lineTo(95,47);
+    ctx.lineTo(95,75);
+    ctx.closePath();
+    ctx.fillStyle = fontColor;
+    ctx.fill();
 });
