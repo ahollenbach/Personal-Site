152995ee0434c7aa6ed08f7d7bceb13d4c5bf930
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2014-09-28 11:58:51 -0400

    Added YourOnlyFriend page

---
+++ yo/youronlyfriend/index.php
@@ -0,0 +1,16 @@
+<?php 
+	$title = "YourOnlyFriend";
+    include("header.php"); 
+?>
+
+<h1><span class="highlight">YourOnlyFriend</span></h1>
+
+<h4>No friends on Yo? No worries, just add YourOnlyFriend.</h4>
+
+<p>If you don't have friends on Yo, you now have one! Just Yo YourOnlyFriend whenever you're feeling lonely, and your new friend will yo you back in a reasonable amount of time!</p>
+
+<p>To subscribe, simply Yo YOURONLYFRIEND!</p>
+
+<p>Not sure what Yo is? <a href="http://www.justyo.co/">Check it out here.</a></p>
+
+<?php include("footer.php"); ?>
